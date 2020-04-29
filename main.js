598c06fdaa3c125d84423b44dae5f970e0f9b20a
Konpaku Youmu <saber2pr@gmail.com>
2020-04-29 18:37:33 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// 1. config requirejs
+// --- or import lib from url, like
+// --- import React from 'https://cdn.bootcss.com/react/16.13.1/umd/react.production.min.js'
+const oldLoad = requirejs.load
+requirejs.load = function (context, id, url) {
+  if (id === 'react') {
+    url = 'https://cdn.bootcss.com/react/16.13.1/umd/react.production.min.js'
+  }
+
+  if (id === 'react-dom') {
+    url = 'https://cdn.bootcss.com/react-dom/16.13.1/umd/react-dom.production.min.js'
+  }
+
+  return oldLoad.call(requirejs, context, id, url)
+}
+
+// 2. enjoy it!
+console.log('o(*≧▽≦)ツ┏━┓', new Date().toLocaleString())
+
